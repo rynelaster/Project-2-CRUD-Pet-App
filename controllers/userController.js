6281309705224ecb0ffdc7d96a45913932539c67
Ryne Laster <ryne.laster@gmail.com>
2018-01-23 09:56:01 -0600

    login and registration pages have been added. method overrride was installed and required.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,64 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const User = require('../models/users.js');
 
 //remember to require the Model here
 
-router.route('/')
-	.get((req, res) => {
+// router.route('/login')
+// 	.get((req, res) => {
 
-		res.send('log in fam');
+// 		res.send('log in fam');
+// 	})
+
+router.get('/login', (req, res =>{
+	res.render('/login', (req, res)=>{
+		User.findOne(username: req.body.username), (err, user)=>{
+
+			if(user){
+
+				if(bcrypt.compareSync(req.body.password, user.password)){
+					req.session.username = req.body.username;
+					req.session.logged: true;
+					req.session.message = '';
+
+					res.redirect('/home')
+
+				}else{
+					req.session.message = "Username or Password are incorrect"
+					res.redirect('/users/login')
+				}
+
+			}else{
+				console.log(err, 'there was an error')
+				req.session.message = "Username or Password are incorrect"
+				
+			}
+		}
 	})
+}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = router;
